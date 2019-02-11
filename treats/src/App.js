433d4ed22d1f47144ddb283059759de81df65052
samjeffcoat <samjeffcoat@gmail.com>
2019-02-11 15:24:52 -0600

    Sets up Photo components for project

--- treats/src/App.js
+++ treats/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Photo from "./components/Photo/Photo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         trEATs 
+        <div>
+          <Photo/>
+        </div>
       </div>
     );
   }
---
+++ treats/src/components/Photo/Photo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Photo = props => {
+    return (
+        <div className= "photo-card">
+        <div className= "img-container">
+                <img className= "photo" src= "http://thenaughtyfork.com/wp-content/uploads/2018/10/IMG_1037.jpg" alt= "pasta" />
+        </div>
+        </div>
+    )
+}
+
+export default Photo;
