62e905ad52adfef22a2079a275b55b305f9a9aa7
samjeffcoat <samjeffcoat@gmail.com>
2019-02-15 13:29:09 -0600

    Changes home page up a little

--- treats/src/App.js
+++ treats/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, withRouter, NavLink } from "react-router-dom";
+import { Route, withRouter, NavLink, Link } from "react-router-dom";
 import "./App.css";
 import LogIn from "./components/Login/Login";
 import SignUp from "./components/SignUpForm/SignUp";
@@ -7,47 +7,76 @@ import Post from "./components/Post/Post";
 import ArtistsView from "./components/ArtistsView/ArtistsView";
 import styled, { css } from "styled-components";
 
-const Wrapper = styled.section`
+const NavWrapper = styled.section`
   color: palevioletred;
-  padding: 4em;
   background: papayawhip;
+  height: 30px;
+  display: flex;
+  justify-content: space-around;
+  flex
+  .nav: active {
+    border-bottom: 1px solid blue;
+  }
 `;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Wrapper>
-          <header className="App-Header">
-            <h1>Welcome to trEATs</h1>
-            <nav className="main-nav">
-              <NavLink className="app-link" to="/artists" co exact>
-                Artists
-              </NavLink>
-              <NavLink
-                onClick={this.logout}
-                className="app-link"
-                to="/login"
-                exact
-              >
-                Log Out
-              </NavLink>
-              <NavLink className="app-link" to="/login" exact>
-                Login
-              </NavLink>
-              <NavLink className="app-link" to="/signup" exact>
-                SignUp
-              </NavLink>
-            </nav>
-          </header>
-          <Route exact path="/login" component={LogIn} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/post" component={Post} />
-          <Route exact path="/artists" component={ArtistsView} />
-        </Wrapper>
+        <NavWrapper>
+          <li>
+            <NavLink className="app-link" to="/artists" exact>
+              Artists
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="app-link" to="/login" exact>
+              Login
+            </NavLink>
+          </li>
+        </NavWrapper>
+        <Route path="/" component={Home} />
+        <Route path="/login" component={LogIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/post" component={Post} />
+        <Route path="/artists" component={ArtistsView} />
       </div>
     );
   }
 }
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to trEATs</h1>
+      <h4>Please Login Below: </h4>
+      <Link to="/login">Login</Link>
+    </div>
+  );
+}
+/*
+      <Route exact path="/login" component={LogIn} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/post" component={Post} />
+      <Route exact path="/artists" component={ArtistsView} />
+*/
 
 export default withRouter(App);
+/*
+<Route exact path="/login" component={LogIn} />
+  <Route exact path="/signup" component={SignUp} />
+  <Route exact path="/post" component={Post} />
+  <Route exact path="/artists" component={ArtistsView} />
+  
+*/
+
+/*
+Logout function for later use
+onClick={this.logout}
+              className="app-link"
+              to="/login"
+              exact
+            >
+              Log Out
+
+
+*/
